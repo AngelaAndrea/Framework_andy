f280f3cc08edfbb91afe1bd5dc604fd6cf5dd9b1
AngelaAndrea <47572006+AngelaAndrea@users.noreply.github.com>
2025-04-29 14:09:57 -0600

    Primer commit con pruebas de youtube funcionando

---
+++ codecept.conf.js
@@ -0,0 +1,28 @@
+const { setHeadlessWhen, setCommonPlugins } = require('@codeceptjs/configure');
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+// enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
+setCommonPlugins();
+
+/** @type {CodeceptJS.MainConfig} */
+exports.config = {
+  tests: './youtubeAutomatizacion.js',
+  output: './output',
+  helpers: {
+    Playwright: {
+      browser: 'chromium',
+      url: "https://www.youtube.com",
+      show: true
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  gherkin: {
+    steps: "./steps/youtubeAutomatizacion.feature",
+    tests: ["./tests/youtubeAutomatizacion.js"],
+  }, 
+  name: 'framework_andy',
+}
